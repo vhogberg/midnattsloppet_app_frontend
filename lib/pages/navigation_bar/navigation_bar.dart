802b05c325339c74933a1d769c2435d4fd54a141
Gustav Dahlberg <Gustav.dahlberg951@gmail.com>
2024-05-06 16:35:09 +0200

    Added myteampage and updated navbar

--- lib/pages/navigation_bar/navigation_bar.dart
+++ lib/pages/navigation_bar/navigation_bar.dart
@@ -3,6 +3,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:flutter_application/pages/homepage.dart';
 import 'package:flutter_application/pages/searchpage.dart';
+import 'package:flutter_application/pages/myteampage.dart';
 //import 'package:flutter_application/pages/examplepage1.dart';
 //import 'package:flutter_application/pages/examplepage2.dart';
 //import 'package:flutter_application/pages/examplepage3.dart';
@@ -79,7 +80,7 @@ class _NavigationBarState extends State<MyNavigationBar> {
                     ),
                     const SizedBox(width: 10),
                     IconButton(
-                      onPressed: () => _selectPage(3),
+                      onPressed: () => _goToMyTeamPage(context),
                       icon: Icon(
                         _icons[3],
                         size: _iconSize,
@@ -155,6 +156,12 @@ class _NavigationBarState extends State<MyNavigationBar> {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => SearchPage()),
+    );
+}
+    void _goToMyTeamPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MyTeamPage()),
     );
   }
 }
