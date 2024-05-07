4ab168749a16ed28582ac94f0053777455689eff
thheemanuel <tgyllner@gmail.com>
2024-05-07 19:43:15 +0200

    sökfunktion för notiserna

--- lib/pages/notification_page/notification_page.dart
+++ lib/pages/notification_page/notification_page.dart
@@ -8,7 +8,11 @@ class NotificationItem {
   final DateTime time;
   bool isRead; // Spåra om notisen är läst eller inte
 
-  NotificationItem({required this.title, required this.message, required this.time, this.isRead = false});
+  NotificationItem(
+      {required this.title,
+      required this.message,
+      required this.time,
+      this.isRead = false});
 }
 
 class NotificationPage extends StatefulWidget {
@@ -16,9 +20,14 @@ class NotificationPage extends StatefulWidget {
   _NotificationPageState createState() => _NotificationPageState();
 }
 
-class _NotificationPageState extends State<NotificationPage> with SingleTickerProviderStateMixin {
+class _NotificationPageState extends State<NotificationPage>
+    with SingleTickerProviderStateMixin {
   late SharedPreferences _prefs; // SharedPreferences-instans
-  late TabController _tabController; // TabController för att hantera TabBar och TabBarView
+  late TabController
+      _tabController; // TabController för att hantera TabBar och TabBarView
+  final TextEditingController _searchController =
+      TextEditingController(); // Controller för sökfältet
+  String _searchTerm = ''; // Söktermen
 
   // simulera notifikationer
   List<NotificationItem> allNotifications = [
@@ -29,31 +38,35 @@ class _NotificationPageState extends State<NotificationPage> with SingleTickerPr
     ),
     NotificationItem(
       title: "30% av donationsmålet uppnått!",
-      message: "Ni har uppnått 30% av erat donationsmål! \nGrattis och fortsätt!",
+      message:
+          "Ni har uppnått 30% av erat donationsmål! \nGrattis och fortsätt!",
       time: DateTime.now(),
     ),
     NotificationItem(
       title: "60% av donationsmålet uppnått!",
-      message: "Ni har uppnått 60% av erat donationsmål! \nGrattis och fortsätt!",
+      message:
+          "Ni har uppnått 60% av erat donationsmål! \nGrattis och fortsätt!",
       time: DateTime.now(),
     ),
     NotificationItem(
       title: "90% av donationsmålet uppnått",
-      message: "Ni har uppnått 90% av erat donationsmål! \nGrattis och fortsätt!",
+      message:
+          "Ni har uppnått 90% av erat donationsmål! \nGrattis och fortsätt!",
       time: DateTime.now(),
     ),
     NotificationItem(
       title: "50 dagar kvar till loppet",
-      message: "Det är 50 dagar kvar till midnattsloppets racestart! \nSpara datumet: 27 Augusti 2024",
+      message:
+          "Det är 50 dagar kvar till midnattsloppets racestart! \nSpara datumet: 27 Augusti 2024",
       time: DateTime.now(),
     ),
     NotificationItem(
       title: "100 dagar kvar till loppet",
-      message: "Det är 100 dagar klvar till midnattsloppets racestart! \nSpara datumet: 27 Augusti 2024",
+      message:
+          "Det är 100 dagar klvar till midnattsloppets racestart! \nSpara datumet: 27 Augusti 2024",
       time: DateTime.now(),
     ),
-    
-    // Lägg till fler notifikationer här vid behov
+    // Andra notifikationer här ...
   ];
 
   List<NotificationItem> unreadNotifications = [];
@@ -62,13 +75,15 @@ class _NotificationPageState extends State<NotificationPage> with SingleTickerPr
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this); // Skapa en TabController med 3 flikar
+    _tabController = TabController(
+        length: 3, vsync: this); // Skapa en TabController med 3 flikar
     initializeSharedPreferences();
   }
 
   @override
   void dispose() {
     _tabController.dispose(); // Ta bort TabController när widgeten förstörs
+    _searchController.dispose(); // Ta bort TextEditingController
     super.dispose();
   }
 
@@ -110,6 +125,71 @@ class _NotificationPageState extends State<NotificationPage> with SingleTickerPr
     _prefs.setBool(notification.title, notification.isRead);
   }
 
+  // Funktion för att filtrera notifikationer baserat på söktermen
+  List<NotificationItem> filterNotifications(String searchTerm) {
+    return allNotifications.where((notification) {
+      return notification.title
+              .toLowerCase()
+              .contains(searchTerm.toLowerCase()) ||
+          notification.message.toLowerCase().contains(searchTerm.toLowerCase());
+    }).toList();
+  }
+
+  // Funktion för att visa modalbottenpanelen för sökning
+  void _showSearchModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Spacer(),
+                  IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      _searchController.clear();
+                      setState(() {
+                        _searchTerm = '';
+                      });
+                    },
+                  ),
+                ],
+              ),
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  labelText: 'Sök',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _searchTerm = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(fontFamily: 'Nunito'), // Ändra fonten för texten på knappen
+                ),
+                onPressed: () {
+                  Navigator.pop(context); // Stäng modalbottenpanelen
+                  // Utför sökningen här om det behövs
+                },
+                child: Text('Sök'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -122,11 +202,15 @@ class _NotificationPageState extends State<NotificationPage> with SingleTickerPr
           ),
         ),
         actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              // Sökfunktionalitet här
-            },
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                _showSearchModal(
+                    context); // Visa modalbottenpanelen för sökning
+              },
+            ),
           ),
         ],
         bottom: TabBar(
@@ -144,9 +228,24 @@ class _NotificationPageState extends State<NotificationPage> with SingleTickerPr
             child: TabBarView(
               controller: _tabController, // Använd vår TabController
               children: [
-                NotificationList(notifications: allNotifications, updateStatus: updateNotificationStatus),
-                NotificationList(notifications: unreadNotifications, updateStatus: updateNotificationStatus),
-                NotificationList(notifications: readNotifications, updateStatus: updateNotificationStatus),
+                NotificationList(
+                  notifications: _searchTerm.isEmpty
+                      ? allNotifications
+                      : filterNotifications(_searchTerm),
+                  updateStatus: updateNotificationStatus,
+                ),
+                NotificationList(
+                  notifications: _searchTerm.isEmpty
+                      ? unreadNotifications
+                      : filterNotifications(_searchTerm),
+                  updateStatus: updateNotificationStatus,
+                ),
+                NotificationList(
+                  notifications: _searchTerm.isEmpty
+                      ? readNotifications
+                      : filterNotifications(_searchTerm),
+                  updateStatus: updateNotificationStatus,
+                ),
               ],
             ),
           ),
@@ -158,9 +257,11 @@ class _NotificationPageState extends State<NotificationPage> with SingleTickerPr
 
 class NotificationList extends StatelessWidget {
   final List<NotificationItem> notifications;
-  final Function(NotificationItem) updateStatus; // Funktion för att uppdatera notisstatus
+  final Function(NotificationItem)
+      updateStatus; // Funktion för att uppdatera notisstatus
 
-  const NotificationList({required this.notifications, required this.updateStatus});
+  const NotificationList(
+      {required this.notifications, required this.updateStatus});
 
   @override
   Widget build(BuildContext context) {
@@ -177,11 +278,13 @@ class NotificationList extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => NotificationDetail(notification: notifications[index]),
+                builder: (context) =>
+                    NotificationDetail(notification: notifications[index]),
               ),
             ).then((value) {
               // Återställ listorna när användaren återvänder från notisdetaljsidan
-              _NotificationPageState notificationPageState = context.findAncestorStateOfType<_NotificationPageState>()!;
+              _NotificationPageState notificationPageState =
+                  context.findAncestorStateOfType<_NotificationPageState>()!;
               notificationPageState.resetLists();
             });
           },
@@ -266,3 +369,7 @@ class NotificationDetail extends StatelessWidget {
     );
   }
 }
+
+
+
+
