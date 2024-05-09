b0d01bdd7a46baa8095ee90ed2abe024fe6a6f5d
thheemanuel <tgyllner@gmail.com>
2024-05-09 14:30:27 +0200

    git strular så in i

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyNavigationBar(),
+      home: LoginPage(),
     );
   }
 }
