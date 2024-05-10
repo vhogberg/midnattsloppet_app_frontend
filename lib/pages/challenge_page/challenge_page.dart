f1d5b4cc010482335e2c1058af46d28f30b43cb8
vhogberg <119532259+vhogberg@users.noreply.github.com>
2024-05-10 17:56:31 +0200

    Search field pop-up in challenge page implemented. Able to select team to challenge now.

--- lib/pages/challenge_page/challenge_page.dart
+++ lib/pages/challenge_page/challenge_page.dart
@@ -1,14 +1,31 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application/components/custom_app_bar.dart';
+import 'package:http/http.dart' as http;
 import 'package:iconsax/iconsax.dart';
 
-class ChallengePage extends StatelessWidget {
-  // Dessa variabler bör ju sättas nån annanstans
-  bool challengeSent =
-      false; // Bool för ifall en användare har skickat en inbjudan till ett annat team
-  bool challengeReceived =
-      true; // Bool för ifall ett annat team har skickat en inbjudan till användaren
-  String senderTeam = 'Nordea lag 3'; // Namnet på teamet som skicka inbjudan
+class ChallengePage extends StatefulWidget {
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  bool challengeSent = false;
+  bool challengeReceived = true;
+  String senderTeam = 'Nordea lag 3';
+
+  TextEditingController searchController = TextEditingController();
+  List<String> teams = [];
+  List<String> filteredTeams = [];
+  String? selectedTeam;
+
+  @override
+  void initState() {
+    super.initState();
+    filteredTeams.addAll(teams);
+    fetchEntitiesFromAPI();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +45,19 @@ class ChallengePage extends StatelessWidget {
                 'images/CompetitionImage.png',
                 fit: BoxFit.fitWidth,
               ),
-              const SizedBox(height: 6), // space mellan nedkant av bild och sökfält
-              const Padding(
-                padding: EdgeInsets.only(top: 10, bottom: 10), // space i topkant och nedkant av sökfält
+              const SizedBox(height: 6),
+
+              // Sökfältknapp
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
                 child: TextField(
+                  controller: searchController,
+                  onChanged: (value) {
+                    filterSearchResults(value);
+                  },
+                  onTap: () {
+                    _showSearchResultsPopup(context);
+                  },
                   decoration: InputDecoration(
                     hintText: 'Sök upp lag att utmana...',
                     prefixIcon: Icon(Icons.search),
@@ -39,49 +65,50 @@ class ChallengePage extends StatelessWidget {
                   ),
                 ),
               ),
-              const SizedBox(height: 4), // space under sökfält och mellan textinstruktionerna
-              const Align(
+              const SizedBox(height: 4),
+              Align(
                 alignment: Alignment.centerLeft,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                      padding: EdgeInsets.only(bottom: 8.0), // space mellan punkt 1-2
+                      padding: const EdgeInsets.only(bottom: 8.0),
                       child: Text(
                         '1. Sök upp ett lag som ni vill starta lagkamp emot',
                         style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'Sora',
-                            fontWeight: FontWeight.bold),
+                          fontSize: 20.0,
+                          fontFamily: 'Sora',
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(bottom: 8.0), // space mellan punkt 2-3
+                      padding: const EdgeInsets.only(bottom: 8.0),
                       child: Text(
                         '2. Skicka iväg utmaningen och invänta svar från det andra laget',
                         style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'Sora',
-                            fontWeight: FontWeight.bold),
+                          fontSize: 20.0,
+                          fontFamily: 'Sora',
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(bottom: 6.0), // space mellan punkt 3 och skicka-knapp
+                      padding: const EdgeInsets.only(bottom: 6.0),
                       child: Text(
                         '3. En aktiv lagkamp startas där ni kan tävla i donationer och skapa egna mini-tävlingar!',
                         style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'Sora',
-                            fontWeight: FontWeight.bold),
+                          fontSize: 20.0,
+                          fontFamily: 'Sora',
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
-
-              // Skicka iväg utmaning-knapp
               Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6), // igen, space ovanför skicka-knapp, lite dubbelfunktion här.
+                padding: const EdgeInsets.symmetric(vertical: 6),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -122,10 +149,6 @@ class ChallengePage extends StatelessWidget {
                   ],
                 ),
               ),
-
-// test 2
-
-              // Inboxlåda för utmaningar
               Container(
                 margin: const EdgeInsets.symmetric(vertical: 12.0),
                 padding: const EdgeInsets.all(20.0),
@@ -137,7 +160,6 @@ class ChallengePage extends StatelessWidget {
                     ? Column(
                         children: [
                           Text(
-                            // senderTeam byts ut baserat på variabeln längst upp
                             '$senderTeam vill starta en lagkamp med er, acceptera?',
                             style: const TextStyle(
                               fontSize: 16.0,
@@ -150,7 +172,6 @@ class ChallengePage extends StatelessWidget {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              // Acceptera lagkamp knapp
                               ElevatedButton(
                                 onPressed: () {
                                   // Acceptera lagkamp logik här
@@ -168,11 +189,10 @@ class ChallengePage extends StatelessWidget {
                                       style: TextStyle(
                                         fontSize: 16,
                                         fontWeight: FontWeight.bold,
-                                        color: Colors.black, // Black text color
+                                        color: Colors.black,
                                       ),
                                     ),
-                                    SizedBox(
-                                        width: 8), // Space mellan text och ikon
+                                    SizedBox(width: 8),
                                     Icon(
                                       Iconsax.like_1,
                                       size: 24,
@@ -182,8 +202,6 @@ class ChallengePage extends StatelessWidget {
                                 ),
                               ),
                               const SizedBox(width: 20),
-
-                              // Avböj lagkamp knapp
                               ElevatedButton(
                                 onPressed: () {
                                   // Avböj lagkamp logik här
@@ -204,8 +222,7 @@ class ChallengePage extends StatelessWidget {
                                         color: Colors.black,
                                       ),
                                     ),
-                                    SizedBox(
-                                        width: 8), // space mellan text och ikon
+                                    SizedBox(width: 8),
                                     Icon(
                                       Iconsax.dislike,
                                       size: 24,
@@ -218,8 +235,6 @@ class ChallengePage extends StatelessWidget {
                           ),
                         ],
                       )
-
-                    // om användaren har skickat challenge, visa detta
                     : (challengeSent
                         ? Text(
                             'Inväntar svar från lag $senderTeam',
@@ -230,8 +245,6 @@ class ChallengePage extends StatelessWidget {
                             ),
                             textAlign: TextAlign.center,
                           )
-
-                        // Om användaren inte har skickat challenge, och ingen challenge har blivit skickat till dem, visa detta:
                         : const Text(
                             'Inga aktiva inbjudningar eller förfrågningar',
                             style: TextStyle(
@@ -247,5 +260,115 @@ class ChallengePage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  // Sökresultat pop-up ruta när man klickar sökfältet.
+  void _showSearchResultsPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return Dialog(
+              insetPadding: EdgeInsets.all(20.0),
+              child: WillPopScope(
+                onWillPop: () async {
+                  searchController.clear();
+                  return true;
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8 + 45, // bredd pop-up
+                  height: MediaQuery.of(context).size.height * 0.5, // höjd pop-up
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: TextField(
+                          controller: searchController,
+                          decoration: InputDecoration(
+                            hintText: 'Ange lagnamn...',
+                            prefixIcon: Icon(Icons.search),
+                            border: OutlineInputBorder(),
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              filterSearchResults(value);
+                            });
+                          },
+                        ),
+                      ),
+                      // Generera lista av teams inuti pop-up
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: filteredTeams.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text(
+                                filteredTeams[index],
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  selectedTeam = filteredTeams[index];
+                                });
+                                Navigator.pop(context);
+                              },
+                              selected: selectedTeam == filteredTeams[index],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    ).then((value) {
+      // Töm tidigare sökningar när pop-up stängs eller team väljs
+      searchController.clear();
+    });
+
+    // Töm tidigare filtrerade teams
+    filteredTeams.clear();
+    // Lägg till alla teams igen
+    filteredTeams.addAll(teams);
+  }
+
+// Metod för att filtrera sökresultat
+  void filterSearchResults(String query) {
+    List<String> searchResults = [];
+    if (query.isNotEmpty) {
+      for (String team in teams) {
+        if (team.toLowerCase().contains(query.toLowerCase())) {
+          searchResults.add(team);
+        }
+      }
+    } else {
+      searchResults.addAll(teams);
+    }
+    setState(() {
+      filteredTeams = searchResults;
+    });
+  }
+
+// Hämta lag från API
+  Future<void> fetchEntitiesFromAPI() async {
+    final response = await http
+        .get(Uri.parse('https://group-15-7.pvt.dsv.su.se/app/all/teams'));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      setState(() {
+        teams = List<String>.from(data);
+        filteredTeams.addAll(teams);
+      });
+    } else {
+      throw Exception('Failed to load teams from API');
+    }
   }
 }
