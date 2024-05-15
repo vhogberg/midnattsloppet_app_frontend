4644e8100f5d109946fc7922d37be5ae284b3503
thheemanuel <tgyllner@gmail.com>
2024-05-15 14:24:05 +0200

    darwin ios osv

--- lib/api_utils/notification_api.dart
+++ lib/api_utils/notification_api.dart
@@ -5,27 +5,31 @@ class NotificationApi {
   static final _notifications = FlutterLocalNotificationsPlugin();
   static final onNotifications = BehaviorSubject<String?>();
 
-  static Future _notificationDetails() async {
+  static Future<NotificationDetails> _notificationDetails() async {
     return const NotificationDetails(
       android: AndroidNotificationDetails(
         'channel id',
         'channel name',
-        'channel description',
+        channelDescription: 'channel description',
         importance: Importance.max,
       ),
-      iOS: IOSNotificationDetails(),
+      iOS: DarwinNotificationDetails(),
     );
   }
 
   static Future init({bool initScheduled = false}) async {
     final android = AndroidInitializationSettings('@mipmap/ic_launcher');
-    final iOS = IOSInitializationSettings();
+    final iOS = DarwinInitializationSettings(
+      onDidReceiveLocalNotification: (id, title, body, payload) async {
+        // Handle iOS foreground notification
+      },
+    );
     final settings = InitializationSettings(android: android, iOS: iOS);
 
     await _notifications.initialize(
       settings,
-      onSelectNotification: (payload) async {
-        onNotifications.add(payload);
+      onDidReceiveNotificationResponse: (NotificationResponse response) async {
+        onNotifications.add(response.payload);
       },
     );
   }
