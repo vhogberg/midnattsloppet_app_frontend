2b2b71457705cb60ac8f3075127d7c6314a91713
Gustav Dahlberg <Gustav.dahlberg951@gmail.com>
2024-05-17 11:14:40 +0200

    modyfied myteampage to be scrollable

--- lib/pages/myteampage.dart
+++ lib/pages/myteampage.dart
@@ -182,237 +182,240 @@ class _MyTeamPageState extends State<MyTeamPage> {
           SessionManager.instance.signUserOut(context);
         },
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Display team name above the circular team picture
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Center(
-              child: Text(
-                '$teamName',
-                style: TextStyle(
-                  fontSize: 20, // Adjust font size
-                  color: Colors.black, // Set color to black
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Sora',
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-              height: 10), // Add some spacing between teamName and the picture
-          // Circular team picture at the middle top
-          Center(
-            child: Container(
-              width: 150,
-              height: 150,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color(0XFF3C4785), // Use the blue color provided
-              ),
-              child: const Center(
-                child: CircleAvatar(
-                  radius: 74,
-                  backgroundColor: Colors.white,
-                  backgroundImage:
-                      AssetImage('images/stockholm-university.png'),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          // Textbox with the donation pledge name
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Center(
-              child: Column(
-                children: [
-                  Text(
-                    'Företag: $companyName', // Display the company name
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Sora',
-                    ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Display team name above the circular team picture
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Center(
+                child: Text(
+                  '$teamName',
+                  style: TextStyle(
+                    fontSize: 20, // Adjust font size
+                    color: Colors.black, // Set color to black
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Sora',
                   ),
-                  Text(
-                    'Stödjer: $charityName', // Display the charity name
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Sora',
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-
-          // Container with team members name
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              width: MediaQuery.of(context).size.width,
-              height: 500,
-              decoration: BoxDecoration(
-                color: const Color(0XFF3C4785),
-                borderRadius: BorderRadius.circular(12.0),
-                gradient: const RadialGradient(
-                  radius: 0.8,
-                  center: Alignment(-0.5, 0.4),
-                  colors: [
-                    Color.fromARGB(255, 140, 90, 100), // Start color
-                    Color(0xFF3C4785), // End color
-                  ],
-                  stops: [
-                    0.15,
-                    1.0,
-                  ],
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Lagmedlemmar',
-                    style: TextStyle(
-                        color: Colors.white,
+            ),
+            SizedBox(
+                height:
+                    10), // Add some spacing between teamName and the picture
+            // Circular team picture at the middle top
+            Center(
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0XFF3C4785), // Use the blue color provided
+                ),
+                child: const Center(
+                  child: CircleAvatar(
+                    radius: 74,
+                    backgroundColor: Colors.white,
+                    backgroundImage:
+                        AssetImage('images/stockholm-university.png'),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            // Textbox with the donation pledge name
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Företag: $companyName', // Display the company name
+                      style: const TextStyle(
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
-                        fontSize: 28,
-                        fontFamily: 'Sora'),
-                  ),
-                  SizedBox(height: 10),
-                  // Check if members list is empty
-                  members.isEmpty
-                      ? Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            'Inga lagmedlemmar än!',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                fontFamily: 'Sora'),
-                          ),
-                        )
-                      : Column(
-                          // Generate ListTiles dynamically
-                          children: [
-                            ...generateTeamList(),
-                          ],
-                        ),
-                  SizedBox(height: 10),
-                  // Display fetched data
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 130,
-                    decoration: BoxDecoration(
-                      color: Colors.white30,
-                      borderRadius: BorderRadius.circular(13.0),
-                      border: Border.all(
-                        color: Colors.white60, // Border color
-                        width: 1.0, // Border width
+                        fontFamily: 'Sora',
                       ),
                     ),
-                    padding: EdgeInsets.all(10),
-                    child: Stack(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${totalDonations.toStringAsFixed(0)} kr insamlat',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width - 80,
-                              child: const Padding(
-                                padding: EdgeInsets.only(left: 35, right: 35),
-                                child: DonationProgressBar(),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Positioned(
-                          top: 60,
-                          right: 1,
-                          child: GoalBox(height: 50, width: 90),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  // Sharing button
-                  Container(
-                    padding: const EdgeInsets.all(15),
-                    width: MediaQuery.of(context).size.width,
-                    height: 90,
-                    decoration: BoxDecoration(
-                      color: Colors.white30,
-                      borderRadius: BorderRadius.circular(13.0),
-                      border: Border.all(
-                        color: Colors.white60, // Border color
-                        width: 1.0, // Border width
+                    Text(
+                      'Stödjer: $charityName', // Display the charity name
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Sora',
                       ),
                     ),
-                    child: Row(
-                      children: [
-                        const Expanded(
-                          child: Text(
-                            'Dela bössan med vänner och familj!',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Sora'),
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            ShareHelper.showShareDialog(context);
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.all(10),
-                            width: 100,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(13.0),
-                            ),
-                            child: const Row(
-                              children: [
-                                Icon(
-                                  Iconsax.export_1,
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  'Dela',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontFamily: 'Sora',
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 10),
+
+            // Container with team members name
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: const Color(0XFF3C4785),
+                  borderRadius: BorderRadius.circular(12.0),
+                  gradient: const RadialGradient(
+                    radius: 0.8,
+                    center: Alignment(-0.5, 0.4),
+                    colors: [
+                      Color.fromARGB(255, 140, 90, 100), // Start color
+                      Color(0xFF3C4785), // End color
+                    ],
+                    stops: [
+                      0.15,
+                      1.0,
+                    ],
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Lagmedlemmar',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                          fontFamily: 'Sora'),
+                    ),
+                    SizedBox(height: 10),
+                    // Check if members list is empty
+                    members.isEmpty
+                        ? Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              'Inga lagmedlemmar än!',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 28,
+                                  fontFamily: 'Sora'),
+                            ),
+                          )
+                        : Column(
+                            // Generate ListTiles dynamically
+                            children: [
+                              ...generateTeamList(),
+                            ],
+                          ),
+                    SizedBox(height: 10),
+                    // Display fetched data
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        color: Colors.white30,
+                        borderRadius: BorderRadius.circular(13.0),
+                        border: Border.all(
+                          color: Colors.white60, // Border color
+                          width: 1.0, // Border width
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Stack(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '${totalDonations.toStringAsFixed(0)} kr insamlat',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width - 80,
+                                child: const Padding(
+                                  padding: EdgeInsets.only(left: 35, right: 35),
+                                  child: DonationProgressBar(),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Positioned(
+                            top: 60,
+                            right: 1,
+                            child: GoalBox(height: 50, width: 90),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    // Sharing button
+                    Container(
+                      padding: const EdgeInsets.all(15),
+                      width: MediaQuery.of(context).size.width,
+                      height: 90,
+                      decoration: BoxDecoration(
+                        color: Colors.white30,
+                        borderRadius: BorderRadius.circular(13.0),
+                        border: Border.all(
+                          color: Colors.white60, // Border color
+                          width: 1.0, // Border width
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              'Dela bössan med vänner och familj!',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Sora'),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              ShareHelper.showShareDialog(context);
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(10),
+                              width: 100,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(13.0),
+                              ),
+                              child: const Row(
+                                children: [
+                                  Icon(
+                                    Iconsax.export_1,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    'Dela',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: 'Sora',
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
