c5caba0df8c4264d159ccd9206af9059c262e0b3
gabrielbendezu <gabrielbendezu0101@hotmail.com>
2024-05-17 15:50:24 +0200

    importing the component

--- lib/pages/leaderboard_page/leaderboard_page.dart
+++ lib/pages/leaderboard_page/leaderboard_page.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/components/custom_app_bar.dart';
+import 'package:flutter_application/components/top_three_teams.dart'; // Import the new component
 import 'package:flutter_application/models/team.dart';
 import 'package:flutter_application/pages/searchpage.dart';
 import 'package:http/http.dart' as http;
@@ -18,8 +17,7 @@ class LeaderboardPage extends StatefulWidget {
 class _LeaderboardPageState extends State<LeaderboardPage> {
   List<Team> teams = [];
 
-//metod för att hämta "plats X av X till myteampage"
-    int getTeamRank(String teamName) {
+  int getTeamRank(String teamName) {
     for (int i = 0; i < teams.length; i++) {
       if (teams[i].name == teamName) {
         return i + 1;
@@ -52,7 +50,7 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
       body: Center(
         child: Column(
           children: [
-            buildTopThreeTeams(),
+            TopThreeTeams(teams: teams), // Component!
             Expanded(
               child: Container(
                 margin: const EdgeInsets.all(16.0),
@@ -75,125 +73,6 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
           ],
         ),
       ),
-    );
-  }
-
-  Widget buildTopThreeTeams() {
-    if (teams.length < 3) {
-      return const SizedBox
-          .shrink(); // Return an empty widget if there are less than 3 teams
-    }
-
-    List<Team> topThreeTeams = teams.take(3).toList();
-
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Container(
-                    height: 40), // Padding to offset the second place lower
-                buildTeamCircle(topThreeTeams[1], Colors.grey, '2'),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                buildTeamCircle(topThreeTeams[0], Colors.yellow, '1',
-                    isFirstPlace: true),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Container(
-                    height: 40), // Padding to offset the third place lower
-                buildTeamCircle(topThreeTeams[2], Colors.brown, '3'),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildTeamCircle(Team team, Color color, String rank,
-      {bool isFirstPlace = false}) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Column(
-          children: [
-            if (isFirstPlace)
-              const Icon(           
-                Iconsax.crown2,
-                color: Colors.deepPurple,
-                size: 40.0,
-              ),
-            Container(
-              width: isFirstPlace ? 60.0 : 50.0,
-              height: isFirstPlace ? 60.0 : 50.0,
-              decoration: BoxDecoration(
-                color: color,
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.deepPurple,
-                  width: 3.0,
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  rank,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            Text(
-              team.name,
-              style:
-                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              '${team.fundraiserBox} kr', // Ska ha en myntikon
-              style: const TextStyle(fontSize: 14.0),
-            ),
-          ],
-        ),
-        Positioned(
-          bottom: 30.0,
-          child: Container(
-            width: 24.0,
-            height: 24.0,
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Colors.deepPurple,
-                width: 2.0,
-              ),
-            ),
-            child: Center(
-              child: Text(
-                rank,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 
