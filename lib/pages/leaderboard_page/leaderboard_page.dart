e372b7c6654944d84d73931b2dd721e74c91a21e
gabrielbendezu <gabrielbendezu0101@hotmail.com>
2024-05-19 21:41:37 +0200

    Fixed component logic

--- lib/pages/leaderboard_page/leaderboard_page.dart
+++ lib/pages/leaderboard_page/leaderboard_page.dart
@@ -1,10 +1,11 @@
-import 'dart:convert';
+// leaderboard_page.dart
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application/api_utils/api_utils.dart';
 import 'package:flutter_application/components/custom_app_bar.dart';
 import 'package:flutter_application/components/top_three_teams.dart'; // Import the new component
 import 'package:flutter_application/models/team.dart';
 import 'package:flutter_application/pages/searchpage.dart';
-import 'package:http/http.dart' as http;
 import 'package:iconsax/iconsax.dart';
 
 class LeaderboardPage extends StatefulWidget {
@@ -16,6 +17,30 @@ class LeaderboardPage extends StatefulWidget {
 
 class _LeaderboardPageState extends State<LeaderboardPage> {
   List<Team> teams = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTeamsFromAPI();
+  }
+
+  Future<void> fetchTeamsFromAPI() async {
+    try {
+      List<Team> fetchedTeams =
+          await ApiUtils.fetchTeamsWithBoxAndCompanyName();
+      setState(() {
+        teams = fetchedTeams;
+        isLoading = false;
+      });
+    } catch (e) {
+      // Handle any errors that occur during the fetch
+      setState(() {
+        isLoading = false;
+      });
+      print('Error fetching teams: $e');
+    }
+  }
 
   int getTeamRank(String teamName) {
     for (int i = 0; i < teams.length; i++) {
@@ -24,12 +49,6 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
       }
     }
     return -1; // Return -1 if the team is not found
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchTeamsFromAPI();
   }
 
   @override
@@ -47,73 +66,36 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
           );
         },
       ),
-      body: Center(
-        child: Column(
-          children: [
-            TopThreeTeams(teams: teams), // Component!
-            Expanded(
-              child: Container(
-                margin: const EdgeInsets.all(16.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                ),
-                child: ListView.builder(
-                  itemCount: teams.length,
-                  itemBuilder: (context, index) {
-                    return TeamListItem(
-                      ranking: index + 1,
-                      team: teams[index],
-                    );
-                  },
-                ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+                children: [
+                  const TopThreeTeams(), // Component!
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(16.0),
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius:
+                            BorderRadius.circular(16.0), // Rounded corners
+                      ),
+                      child: ListView.builder(
+                        itemCount: teams.length,
+                        itemBuilder: (context, index) {
+                          return TeamListItem(
+                            ranking: index + 1,
+                            team: teams[index],
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
     );
-  }
-
-  Future<void> fetchTeamsFromAPI() async {
-    final response = await http.get(
-        Uri.parse('https://group-15-7.pvt.dsv.su.se/app/all/teamswithbox'));
-
-    if (response.statusCode == 200) {
-      final List<dynamic> data = jsonDecode(response.body);
-      print(data); // Add this line to print the JSON response
-
-      List<Team> fetchedTeams = [];
-
-      for (var item in data) {
-        String name = item['name'];
-        int fundraiserBox = item['fundraiserBox'];
-        String? companyName;
-
-        if (item['company'] != null) {
-          companyName = item['company']['name'];
-        }
-
-        fetchedTeams.add(Team(
-          name: name,
-          fundraiserBox: fundraiserBox,
-          companyName: companyName,
-        ));
-      }
-
-      fetchedTeams = sortTeams(fetchedTeams); // Sort the fetched teams
-      setState(() {
-        teams = fetchedTeams; // Update the state with the sorted teams
-      });
-    } else {
-      throw Exception('Failed to load teams from API');
-    }
-  }
-
-  List<Team> sortTeams(List<Team> teams) {
-    teams.sort((a, b) => b.fundraiserBox.compareTo(a.fundraiserBox));
-    return teams;
   }
 }
 
