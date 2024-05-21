1882eb3f6381fdc8886e8f77d729cfd14e89fd8a
Gustav Dahlberg <Gustav.dahlberg951@gmail.com>
2024-05-21 22:27:18 +0200

    searchpage update so you can click teams

--- lib/pages/searchpage.dart
+++ lib/pages/searchpage.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/api_utils/api_utils.dart';
+import 'package:flutter_application/models/team.dart';
+import 'package:flutter_application/pages/myteampage.dart';
+import 'package:flutter_application/pages/otherteampage.dart';
+import 'package:flutter_application/session_manager.dart';
+import 'package:flutter_application/components/return_arrow_argument.dart';
 
 class SearchPage extends StatefulWidget {
-  SearchPage({Key? key}) : super(key: key);
+  const SearchPage({Key? key}) : super(key: key);
 
   @override
   _SearchPageState createState() => _SearchPageState();
@@ -10,13 +15,78 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController searchController = TextEditingController();
-  List<String> teams = [];
-  List<String> filteredTeams = [];
+  List<Team> teams = [];
+  List<Team> filteredTeams = [];
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
     fetchTeams();
+  }
+
+  Future<void> fetchTeams() async {
+    try {
+      List<Team> fetchedTeams =
+          await ApiUtils.fetchTeamsWithBoxAndCompanyName();
+      setState(() {
+        teams = fetchedTeams;
+        filteredTeams = fetchedTeams;
+        isLoading = false;
+      });
+    } catch (e) {
+      print('Failed to fetch teams: $e');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  void _filterTeams(String searchTerm) {
+    setState(() {
+      filteredTeams = teams
+          .where((team) =>
+              team.name.toLowerCase().contains(searchTerm.toLowerCase()))
+          .toList();
+    });
+  }
+
+  Future<void> _onTeamTap(Team team) async {
+    String? username = SessionManager.instance.username;
+    bool isUserInTeam = false;
+
+    try {
+      List<dynamic>? teamMembers = await ApiUtils.fetchOtherMembers(team.name);
+      if (teamMembers != null) {
+        isUserInTeam =
+            teamMembers.any((member) => member['username'] == username);
+      }
+    } catch (e) {
+      print('Error fetching team members: $e');
+    }
+
+    if (isUserInTeam) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OtherTeamPage(team: team),
+          settings: RouteSettings(
+            arguments: ReturnArrowArgument(showReturnArrow: true),
+          ),
+        ),
+      );
+    } else {
+      // Navigate to OtherTeamPage with the team details
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OtherTeamPage(team: team),
+          settings: RouteSettings(
+            arguments: ReturnArrowArgument(showReturnArrow: true),
+          ),
+        ),
+      );
+    }
   }
 
   @override
@@ -51,69 +121,55 @@ class _SearchPageState extends State<SearchPage> {
               },
             ),
             const SizedBox(height: 16),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                child: ListView.builder(
-                  itemCount: filteredTeams.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(vertical: 8.0),
+            isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : Expanded(
+                    child: Container(
                       padding: const EdgeInsets.all(8.0),
                       decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 6.0,
-                            offset: Offset(0, 2),
-                          ),
-                        ],
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(16.0),
                       ),
-                      child: ListTile(
-                        title: Text(
-                          filteredTeams[index],
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+                      child: ListView.builder(
+                        itemCount: filteredTeams.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return InkWell(
+                            onTap: () => _onTeamTap(filteredTeams[index]),
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(vertical: 8.0),
+                              padding: const EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20.0),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black12,
+                                    blurRadius: 6.0,
+                                    offset: Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: ListTile(
+                                title: Text(
+                                  filteredTeams[index].name,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                    '${filteredTeams[index].fundraiserBox}kr'),
+                              ),
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
-              ),
-            ),
+                    ),
+                  ),
           ],
         ),
       ),
     );
-  }
-
-  Future<void> fetchTeams() async {
-    try {
-      final data = await ApiUtils.fetchTeamsFromAPI();
-      setState(() {
-        teams = data;
-        filteredTeams.addAll(teams);
-      });
-    } catch (e) {
-      print('Failed to fetch teams: $e');
-    }
-  }
-
-  void _filterTeams(String searchTerm) {
-    setState(() {
-      filteredTeams = teams
-          .where(
-              (team) => team.toLowerCase().contains(searchTerm.toLowerCase()))
-          .toList();
-    });
   }
 }
